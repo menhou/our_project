9cfa8b76fcf1ddd50735b39eae518f2bfb81f17a
menhou <mthouminer@gmail.com>
2016-12-13 16:06:20 +0200

    before content provider

--- app/src/main/java/com/example/mhoumine/our_project/AddActivitiesActivity.java
+++ app/src/main/java/com/example/mhoumine/our_project/AddActivitiesActivity.java
@@ -2,12 +2,45 @@ package com.example.mhoumine.our_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class AddActivitiesActivity extends AppCompatActivity {
+import com.example.mhoumine.our_project.model.entities.activityType;
 
+import java.util.ArrayList;
+
+public class AddActivitiesActivity extends AppCompatActivity implements OnItemSelectedListener {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_activities);
+        setTitle("Add Activity");
+        Spinner spinner = (Spinner) findViewById(R.id.typeActivitySpinner);
+        spinner.setPrompt("Choose Activity Type");
+        spinner.setOnItemSelectedListener(this);
+        ArrayList<String> items = new ArrayList<String>();
+        items.add(activityType.ENTERTAINMENT_SHOW.toString());
+        items.add(activityType.FLIGHT.toString());
+        items.add(activityType.TRIP.toString());
+        items.add(activityType.HOTEL_RESORT.toString());
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/mhoumine/our_project/model/SharedPreference/SaveSharedPreference.java
+++ app/src/main/java/com/example/mhoumine/our_project/model/SharedPreference/SaveSharedPreference.java
@@ -5,7 +5,7 @@ import android.content.SharedPreferences;
 /**
  * Created by mhoumine on 11/12/2016.
  */
-
-public class SaveSharedPreference extends SharedPreferences{
-
-}
+//
+//public class SaveSharedPreference extends SharedPreferences{
+//
+//}
