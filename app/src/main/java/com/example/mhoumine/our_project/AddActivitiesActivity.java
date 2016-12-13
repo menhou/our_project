507deaa294de7acd26aa9809fff1a353c053276c
menhou <mthouminer@gmail.com>
2016-12-13 18:20:31 +0200

    before content provider

--- app/src/main/java/com/example/mhoumine/our_project/AddActivitiesActivity.java
+++ app/src/main/java/com/example/mhoumine/our_project/AddActivitiesActivity.java
@@ -1,11 +1,14 @@
 package com.example.mhoumine.our_project;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -14,8 +17,12 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import com.example.mhoumine.our_project.model.entities.activityType;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AddActivitiesActivity extends AppCompatActivity implements OnItemSelectedListener {
+    int year_x,month_x,day_x;
+    static final int DIALOG_ID = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,10 @@ public class AddActivitiesActivity extends AppCompatActivity implements OnItemSe
         Spinner spinner = (Spinner) findViewById(R.id.typeActivitySpinner);
         final EditText editStart = (EditText) findViewById(R.id.StartDateEditText);
         final EditText editEnd = (EditText) findViewById(R.id.EndDateEditText);
+        final Calendar cal = Calendar.getInstance();
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
 
         spinner.setPrompt("Choose Activity Type");
         spinner.setOnItemSelectedListener(this);
@@ -46,7 +57,7 @@ public class AddActivitiesActivity extends AppCompatActivity implements OnItemSe
 
                 if(event.getAction() == MotionEvent.ACTION_UP) {
                     if(event.getRawX() >= (editStart.getRight() - editStart.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                        // insert datepickerdialog
+                        showDialog(DIALOG_ID);
                         return true;
                     }
                 }
@@ -64,4 +75,24 @@ public class AddActivitiesActivity extends AppCompatActivity implements OnItemSe
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(id == DIALOG_ID){
+            return new DatePickerDialog(this,dpickListener, year_x, month_x, day_x);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickListener = new DatePickerDialog.OnDateSetListener(){
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            year_x = year;
+            month_x = month;
+            day_x = dayOfMonth;
+            final EditText editStart = (EditText) findViewById(R.id.StartDateEditText);
+            editStart.setText(day_x + "/" + month_x + "/" + year_x);
+        }
+    };
 }
